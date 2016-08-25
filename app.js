fbc1bf29bcc3d9dff6143a97ba6618e74ff48c4f
Bill Rashid <mohammad-rashid@uiowa.edu>
2016-08-24 20:30:20 -0500

    minor styling edits

--- app.js
+++ app.js
@@ -1,87 +1,11 @@
-///**
-// * var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-// * @type {exports|Parsers|module.exports}
-// */
-//
-//var express = require('express');
-//var bodyParser   = require('body-parser');
-//var path = require('path');
-//
-//
-//var num_connected = 0;
-//
-//debugger;
-//
-//var app = require('express')();
-//var server = require('http').createServer(app);
-//var io = require('socket.io').listen(server);
-//
-//var myServer = app.listen(3000, function () {
-//    console.log("App is now listening on port " + myServer.address().port);
-//});
-//
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.json());
-//
-////Loads js/css files from public
-//app.use(express.static(path.join(__dirname, "/public")));
-//
-//debugger;
-//
-////test server
-//app.get('/', function (req, res) {
-//
-//
-//    res.send('Hello World!');
-//
-//
-//});
-//
-//
-////Show number of connected users, etc.
-//app.get('/server', function(req, res) {
-//
-//    debugger;
-//    res.sendFile(__dirname + '/public/server.html');
-//
-//
-//
-//});
-//
-////Alice and Bob specific pages
-//app.get('/alice', function(req, res) {
-//    res.sendFile(__dirname + '/public/alice.html');
-//});
-//
-//app.get('/bob', function(req, res) {
-//    res.sendFile(__dirname + '/public/bob.html');
-//});
-//
-////TODO: Socket.IO handling
-//
-//debugger;
-//
-//io.on('connection', function(socket){
-//
-//    debugger;
-//    console.log('A user connected! Socket: ' + socket);
-//});
-//
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var is_authenticated = false;
-
 var client_ids = [];
 var client_id = "";
 
 var num_connections = 0;
-
-
 
 app.use(express.static(__dirname + '/public'));
 
