ffd0be8702ae61063325408f326691afe55d81cd
Bill Rashid <mohammad-rashid@uiowa.edu>
2016-08-24 20:27:24 -0500

    fixed changes

--- app.js
+++ app.js
@@ -1,90 +1,8 @@
-///**
-// * var app = require('express')();
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-// * @type {exports|Parsers|module.exports}
-// */
-//
-//var express = require('express');
-//var bodyParser   = require('body-parser');
-//var path = require('path');
-//
-//
-//var num_connected = 0;
-//
-//debugger;
-//
-//var app = require('express')();
-//var server = require('http').createServer(app);
-//var io = require('socket.io').listen(server);
-//
-//var myServer = app.listen(3000, function () {
-//    console.log("App is now listening on port " + myServer.address().port);
-//});
-//
-//app.use(bodyParser.urlencoded({extended: true}));
-//app.use(bodyParser.json());
-//
-////Loads js/css files from public
-//app.use(express.static(path.join(__dirname, "/public")));
-//
-//debugger;
-//
-////test server
-//app.get('/', function (req, res) {
-//
-//
-//    res.send('Hello World!');
-//
-//
-//});
-//
-//
-////Show number of connected users, etc.
-//app.get('/server', function(req, res) {
-//
-//    debugger;
-//    res.sendFile(__dirname + '/public/server.html');
-//
-//
-//
-//});
-//
-////Alice and Bob specific pages
-//app.get('/alice', function(req, res) {
-//    res.sendFile(__dirname + '/public/alice.html');
-//});
-//
-//app.get('/bob', function(req, res) {
-//    res.sendFile(__dirname + '/public/bob.html');
-//});
-//
-////TODO: Socket.IO handling
-//
-//debugger;
-//
-//io.on('connection', function(socket){
-//
-//    debugger;
-//    console.log('A user connected! Socket: ' + socket);
-//});
-//
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var is_authenticated = false;
-
 var client_ids = [];
-var alice_id = "";
-var bob_id = "";
-
-var client_names = ["alice", "bob"];
-var num_connections = 0;
-
-
-
 
 app.use(express.static(__dirname + '/public'));
 
@@ -108,6 +26,24 @@ io.on('connection', function(client) {
 
     console.log('Client connected...');
 
+var client_id = "";
+var num_connections = 0;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/server', function(req, res, next){
+    res.sendFile(__dirname + '/public/server.html') ;
+});
+
+app.get('/client', function(req, res, next) {
+    res.sendFile(__dirname + '/public/client.html');
+});
+
+//server listens for connections
+io.sockets.on('connection', function(client) {
+
+    var username = "client";
+    console.log('A client connected...');
     client_ids[num_connections] = client.id;
     num_connections++;
 
@@ -118,6 +54,25 @@ io.on('connection', function(client) {
         bob_id = client_ids[1];
     }
     debugger;
+
+    console.log("Connections: " + num_connections);
+
+    client.on('disconnect', function() {
+        console.log('A client disconnected...');
+        num_connections--;
+        console.log("Connections: " + num_connections);
+        var i = client_ids.indexOf(client.id);
+        client_ids.splice(i, 1);
+    });
+
+    client.emit('init', num_connections);
+    client_id = client_ids[num_connections - 1];
+
+    client.on ('changeUsername', function (msg) {
+        if(msg!="") {
+            username = msg;
+        }
+    });
 
     //server waits for message from any client
     client.on('join', function(data) {
@@ -143,8 +98,9 @@ io.on('connection', function(client) {
         }
 
         client.broadcast.emit('broad', {
-                                            username: username,
-                                            message: data});
+            username: username,
+            message: data
+        });
     });
 
 });
