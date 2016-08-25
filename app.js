62030cb965fa069b34f509dae71ed3f8c649e66d
Bill Rashid <mohammad-rashid@uiowa.edu>
2016-08-24 20:38:35 -0500

    code changes

--- app.js
+++ app.js
@@ -8,52 +8,38 @@ var client_id = "";
 var num_connections = 0;
 
 app.use(express.static(__dirname + '/public'));
-
 app.get('/server', function(req, res, next){
-    debugger;
     res.sendFile(__dirname + '/public/server.html') ;
 });
 
 app.get('/client', function(req, res, next) {
-    debugger;
     res.sendFile(__dirname + '/public/client.html');
 });
-
-
 
 //server listens for connections
 io.sockets.on('connection', function(client) {
     var username = "client";
-
-    console.log('Client connected...');
+    console.log('A client connected...');
 
     client_ids[num_connections] = client.id;
     num_connections++;
     console.log("Connections: "+num_connections);
     client.on('disconnect', function() {
-        console.log('Got disconnect!');
+        console.log('A client disconnected...');
         num_connections--;
         console.log("Connections: "+num_connections);
         var i = client_ids.indexOf(client.id);
         client_ids.splice(i, 1);
     });
 
-
     client.emit('init', num_connections);
-
-
-    client_id = client_ids[num_connections-1];
-
-
-    debugger;
-
+    client_id = client_ids[num_connections - 1];
 
     client.on ('changeUsername', function (msg) {
-        if(msg!="") {
+        if (msg !== "") {
             username = msg;
         }
     });
-
 
     //server waits for message from any client
     client.on('join', function(data) {
@@ -62,18 +48,12 @@ io.sockets.on('connection', function(client) {
 
     //when server receives messages from either client
     client.on('messages', function(data) {
-
         console.log("Message passing through: " + data);
-
-
-
         client.broadcast.emit('broad', {
             username: username,
             message: data});
     });
 
 });
-
-
 
 server.listen(4200);
